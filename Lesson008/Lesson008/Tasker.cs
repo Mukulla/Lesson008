82eaca3fb51616b9377f7491c6934f251dc34021
Maxim <mbzozn@mail.ru>
2021-03-16 18:52:32 +0300

    Sort Added

--- Lesson008/Lesson008/Tasker.cs
+++ Lesson008/Lesson008/Tasker.cs
@@ -11,6 +11,11 @@ namespace Lesson008
 {
     class Tasker
     {
+        struct Str_Proc
+        {
+            public int ID;
+            public string Name;
+        }
         ConsoleKey SomeKey;
 
         string Spacer = "   ";
@@ -20,6 +25,9 @@ namespace Lesson008
         string[] Names = { "№", "ID", "Name" };
         List<Process> ProceList = new List<Process>();
 
+        List<Str_Proc> SortList = new List<Str_Proc>();
+        bool WasSorted = false;
+
         int Min = 0, Max = 0, Limit = 0, Step = 19, Pages = 0, CurrentPage = 0;
         public Tasker()
         {
@@ -27,7 +35,7 @@ namespace Lesson008
             Limit = PArray001.Length;
             Max = Step;
             Pages = Limit / Step;
-
+           
             foreach (var Pro in PArray001)
             {
                 ProceList.Add(Pro);
@@ -79,13 +87,13 @@ namespace Lesson008
             if (SomeKey == ConsoleKey.DownArrow)
             {
                 ++IndexMarker;
-                MyFunc.CheckLimitataAream(ref IndexMarker, 0, Limit - 1);
+                MyFunc.CheckLimitataAream(ref IndexMarker, Min, Max - 1);
                 return;
             }
             if (SomeKey == ConsoleKey.UpArrow)
             {
                 --IndexMarker;
-                MyFunc.CheckLimitataAream(ref IndexMarker, 0, Limit);
+                MyFunc.CheckLimitataAream(ref IndexMarker, Min, Max - 1);
                 return;
             }
 
@@ -111,8 +119,56 @@ namespace Lesson008
             //Delete Process
             if (SomeKey == ConsoleKey.Delete)
             {
-                ProceList[IndexMarker].Kill();
+                try
+                {
+                    ProceList[IndexMarker].Kill();
+                    ProceList.RemoveAt(IndexMarker);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Failed to delete process ");
+                }                
             }
+
+            if (SomeKey == ConsoleKey.F1)
+            {
+                SortList.Clear();
+
+                var SortedID = from Proc in ProceList
+                               orderby Proc.Id
+                               select Proc;
+                Str_Proc Tempo;
+                foreach (var item in SortedID)
+                {
+                    Tempo.ID = item.Id;
+                    Tempo.Name = item.ProcessName;
+                    SortList.Add(Tempo);
+                }
+
+                WasSorted = true;
+            }
+            if (SomeKey == ConsoleKey.F2)
+            {
+                SortList.Clear();
+
+                var SortedID = from Proc in ProceList
+                               orderby Proc.ProcessName
+                               select Proc;
+                Str_Proc Tempo;
+                foreach (var item in SortedID)
+                {
+                    Tempo.ID = item.Id;
+                    Tempo.Name = item.ProcessName;
+                    SortList.Add(Tempo);
+                }
+                WasSorted = true;
+            }
+
+            if (SomeKey == ConsoleKey.F3)
+            {
+                WasSorted = false;
+            }
+
         }
         public void Show()
         {
@@ -121,20 +177,39 @@ namespace Lesson008
             for (int i = Min; i < Max; ++i)
             {
                 if(i == IndexMarker)
-                {
-                    Console.WriteLine($"{Marker,2}{i,4} {ProceList[i].Id,7} {ProceList[i].ProcessName}");
+                {   
+                    if(WasSorted)
+                    {
+                        Console.WriteLine($"{Marker,2}{i,4} {SortList[i].ID,7} {SortList[i].Name}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{Marker,2}{i,4} {ProceList[i].Id,7} {ProceList[i].ProcessName}");
+                    }                    
                 }
                 else
                 {
-                    Console.WriteLine($"{Spacer,2}{i, 4} {ProceList[i].Id,7} {ProceList[i].ProcessName}");
+                    if (WasSorted)
+                    {
+                        Console.WriteLine($"{Spacer,2}{i,4} {SortList[i].ID,7} {SortList[i].Name}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{Spacer,2}{i,4} {ProceList[i].Id,7} {ProceList[i].ProcessName}");
+                    }
+                    //Console.WriteLine($"{Spacer,2}{i, 4} {ProceList[i].Id,7} {ProceList[i].ProcessName}");
                 }                
             }
 
             Console.WriteLine();
             Console.WriteLine($"Total Pocesses Found: {Limit} | Page: {CurrentPage} Of {Pages}");
-            Console.WriteLine();
-            Console.WriteLine("To Exit Porgram Press Escape");
-            Console.WriteLine("Press PageUp And PageDown To Scroll The List ");
+            Console.WriteLine("Press Escape To Exit Porgram");
+            Console.WriteLine("Press PageUp And PageDown To Scroll The List");
+            Console.WriteLine("Press Up And Down Arrow To Select Process");
+            Console.WriteLine("Press Del To Delete Marked Process");
+            Console.WriteLine("Press F1 To Sort By ID");
+            Console.WriteLine("Press F2 To Sort By Name");
+            Console.WriteLine("Press F3 To Reset Sorting");
         }
     }
 }
