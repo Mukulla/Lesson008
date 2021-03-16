b3da3e1c654e534a22b83af484bd6ddfaa24123d
Maxim <mbzozn@mail.ru>
2021-03-16 19:34:13 +0300

    Easy Lib

---
+++ TryToLib/TryToLib/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using EasyLibe;
+
+namespace TryToLib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Li.Greetings("MA_NAME");
+
+            Console.ReadLine();
+        }
+    }
+}
